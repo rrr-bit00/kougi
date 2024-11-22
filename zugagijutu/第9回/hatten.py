393ac9ae9da454a141599b3d95fe2e09ec36951e
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2024-11-22 11:06:53 +0900

    line35 add "encoding='cp932'". add comment

--- zugagijutu/第9回/hatten.py
+++ zugagijutu/第9回/hatten.py
@@ -1,9 +1,6 @@
 import csv
 
 def judge(syuseki, siken, repo):
-    syuseki = syuseki      
-    siken = siken
-    repo = repo
     if syuseki < 12:
         if syuseki >= 10:
             goukei = siken + repo
@@ -25,22 +22,22 @@ def judge(syuseki, siken, repo):
         
 def csv_file():
     with open('zugagijutu/第9回/hat.csv', 'r', newline='')as r_csv:
-        reader = csv.DictReader(r_csv,delimiter=",")            #csvの中身を読み込む
-        rows = list(reader)         #list型に変換し保存
+        reader = csv.DictReader(r_csv,delimiter=',')                                    #csvの中身を読み込む
+                                                                                        #ヘッダーが","で区切られているため、delimiter=で区切り文字を指定する
+        rows = list(reader)                                                             #list型に変換し保存
         for row in rows:
-            if not row['合否']:         #合否列の空の部分を判定
-                temp_syu = int(row['出席回数'])         #文字から整数型に変換
+            if not row['合否']:                                                         #合否列の空の部分を判定
+                temp_syu = int(row['出席回数'])                                          #文字から整数型に変換
                 temp_si = int(row['試験'])
                 temp_re = int(row['レポート'])
-                row['合否'] = judge(temp_syu,temp_si,temp_re)          #合否を判定するjudge関数を呼び出す
+                row['合否'] = judge(temp_syu,temp_si,temp_re)                           #合否を判定するjudge関数を呼び出す
 
-    with open('zugagijutu/第9回/hat.csv', 'w', newline='')as w_csv:
-        feildnames = reader.fieldnames      #csvを読み込んだ時のreaderを再利用
+    with open('zugagijutu/第9回/hat.csv', 'w', newline='', encoding="cp932")as w_csv:   #2回目以降の実行時に新たに行が追加されているとエラーが起こるため、encoding="cp932"を必ず書く
+        feildnames = reader.fieldnames                                                  #csvを読み込んだ時のreaderを再利用
         writer = csv.DictWriter(w_csv, fieldnames=feildnames)
         writer.writeheader()
         writer.writerows(rows)
-        
-        for row in rows:            #forで1行ずつ出力させる
+        for row in rows:                                                                #forで1行ずつ出力させる
             print(row)
 
 csv_file()
