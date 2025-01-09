8886eecf4775a2488805411b1027dc457fb5740f
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-01-09 10:49:34 +0900

    add code

--- wedプログラミング/第13回/saisyukadai/get.js
+++ wedプログラミング/第13回/saisyukadai/get.js
@@ -2,6 +2,7 @@ function active()
     {
         var hyouzi = document.getElementById('show');
         var text = document.getElementById('inputtext').value;
-            
+        var u_te = encodeURIComponent(text);
+        
             
     }
