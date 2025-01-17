1b2645199a8a074b2cf5018aae49b5007cf731de
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-01-18 01:38:47 +0900

    update .js .hyml create .css

--- wedプログラミング/第13回/saisyukadai/get.js
+++ wedプログラミング/第13回/saisyukadai/get.js
@@ -1,8 +1,17 @@
-function active()
+import {api} from './API.js';
+
+async function active()
     {
         var hyouzi = document.getElementById('show');
-        var text = document.getElementById('inputtext').value;
+        var text = document.getElementById('searchWord').value;
         var u_te = encodeURIComponent(text);
         
-            
+        const src = api + u_te +'&target_lang=JA';
+        const response = await fetch(src);
+        const data = await response.json();
+
+        var show_txt = data.translations[0].text;
+        hyouzi.innerHTML = show_txt;
     }
+
+window.active = active;
