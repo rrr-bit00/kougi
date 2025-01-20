7cfd66032844cb8f593a976d2ded302d5c595eba
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-01-20 11:21:22 +0900

    fixed line8,9,11

--- wedプログラミング/第13回/saisyukadai/get.js
+++ wedプログラミング/第13回/saisyukadai/get.js
@@ -5,10 +5,10 @@ async function active()
         var hyouzi = document.getElementById('show');
         var text = document.getElementById('searchWord').value;
         var u_te = encodeURIComponent(text);
-        var lang = document.getElementById('selectLanguage');
-        var source = document.getElementById('selectSource');
+        var lang = document.getElementById('selectLanguage').value;
+        var source = document.getElementById('selectSource').value;
         
-        var src = api + u_te +'&source_lang='+source.value+'&target_lang='+lang.value+"";
+        var src = api + u_te +'&source_lang='+source+'&target_lang='+lang+"";
         var response = await fetch(src);
         var data = await response.json();
 
