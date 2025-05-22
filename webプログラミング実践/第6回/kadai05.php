51e659098f9b01bcdf0c05ff07a22da4e2590dd0
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-05-22 16:41:37 +0900

    WIP kadai05.php

--- webプログラミング実践/第6回/kadai05.php
+++ webプログラミング実践/第6回/kadai05.php
@@ -1,10 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
     print(
         "<form action='kadai05.php' method='post'>
         <input type='text' name='id' />
+        <input type='submit'/>
         </form>"
     )
 
     $id = htmlspecialchars($_POST['id']);
-    print()
+    print($id);
+
 ?>
+</body>
+</html>
