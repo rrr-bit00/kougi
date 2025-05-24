47d02f97f2de434f215be4f6682a7cf1f8d523fe
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-05-24 22:13:53 +0900

    End of assignment

--- webプログラミング実践/第6回/kadai05.php
+++ webプログラミング実践/第6回/kadai05.php
@@ -1,10 +1,31 @@
-<?php
-    print(
-        "<form action='kadai05.php' method='post'>
-        <input type='text' name='id' />
-        </form>"
-    )
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kadai05</title>
+</head>
+<body>
+    <?php
+    if (empty($_POST['id'])) {
+        print(
+            "<form action='kadai05.php' method='post'>
+            <input type='text' name='id' />
+            <input type='time' name='time' />
+            <input type='submit' />
+            </form>"
+        );
+    }
 
-    $id = htmlspecialchars($_POST['id']);
-    print()
+    else {
+        $post = array_map('htmlspecialchars', $_POST);
+        $name = $post['id'];
+        if ($post['time'] >= '04:00' && $post['time'] < '12:00') $greeting = 'おはよう';
+        if ($post['time'] >= '12:00' && $post['time'] < '20:00') $greeting = 'こんにちは';
+        if ($post['time'] >= '20:00' || $post['time'] < '04:00') $greeting = 'こんばんは';
+
+        print($name.'さん、'.$greeting);
+    }
 ?>
+</body>
+</html>
