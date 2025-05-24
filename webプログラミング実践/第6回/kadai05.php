4b8db8c72a6a86d67eaa9bfc06681eeb8456dfdb
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-05-24 22:26:55 +0900

    コンフリクトの解消

--- webプログラミング実践/第6回/kadai05.php
+++ webプログラミング実践/第6回/kadai05.php
@@ -2,25 +2,6 @@
 <html lang="jp">
 <head>
     <meta charset="UTF-8">
-<<<<<<< HEAD
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    print(
-        "<form action='kadai05.php' method='post'>
-        <input type='text' name='id' />
-        <input type='submit'/>
-        </form>"
-    )
-
-    $id = htmlspecialchars($_POST['id']);
-    print($id);
-
-?>
-</body>
-</html>
-=======
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kadai05</title>
 </head>
@@ -48,4 +29,3 @@
 ?>
 </body>
 </html>
->>>>>>> main
