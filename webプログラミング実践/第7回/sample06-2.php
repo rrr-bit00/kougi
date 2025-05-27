5f037bb0d5a02d3655312d6aa4030b332a398fbe
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-05-27 15:10:41 +0900

    第7回 課題　完了

---
+++ webプログラミング実践/第7回/sample06-2.php
@@ -0,0 +1,37 @@
+<html><head>
+    <title>Sample06-1 Simple Login</title>
+    <?php $PASSWORD = ['user' => 'upass', 'admin' => 'apass']; ?>
+</head>
+
+<body>
+
+<?php
+if( empty($_POST['id']) || empty($_POST['pass']) )
+{
+    print("
+    <form action='' method='post'>
+    user id : <input type='text' name='id'><br />
+    password : <input type='password' name='pass'><br />
+    <input type='submit' text='LOGIN'>
+    </form>
+    ");
+    print('<hr> $PASSWORD 配列の中身 : ');
+    var_dump($PASSWORD);
+}
+else
+{
+    $id = $_POST['id'];         // 毎回 $_POST を書くのが面倒なので、変数で受ける
+    $pass = $_POST['pass'];
+
+    if( $pass == $PASSWORD[$id])  // この行を書き換える
+    {
+        print("$id で LOGIN 成功");
+    }
+    else 
+    {
+        print('LOGIN 失敗');
+    }
+}
+
+?>
+</body></html>
